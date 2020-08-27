063917088f25277a59bf57484ac5225ada8e6141
beckho <vngkgk624@gmail.com>
2020-08-27 23:45:09 +0900

    forutona 관리자 초기화 프로젝트

---
+++ src/main/java/com/wing/backend/forutonamanager/ForutonaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wing.backend.forutonamanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForutonaManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForutonaManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wing/backend/forutonamanager/ForutonaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wing.backend.forutonamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForutonaManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
